aa15ae5e76a80330f4da4f7106d65166a8d4d1ce
강찬욱 <114604267+chanwookK@users.noreply.github.com>
2026-07-18 02:50:11 +0900

    Time: 35 ms (45.25%), Space: 45.6 MB (95.76%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.java
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -1,34 +1,33 @@
 class Solution {
 
-    public int[][] distance;
+    public boolean[][] reachable;
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
-        distance = new int[numCourses][numCourses];
+        reachable = new boolean[numCourses][numCourses];
         for (int i = 0; i < numCourses; i++) {
             for (int j = 0; j < numCourses; j++) {
-                if (i == j)  distance[i][j] = 0;
-                else distance[i][j] = Integer.MAX_VALUE;
+                if (i == j)  reachable[i][j] = true;
+                else reachable[i][j] = false;
             }
         }
 
         for (int[] prerequisite : prerequisites) {
-            distance[prerequisite[0]][prerequisite[1]] = 1;
+            reachable[prerequisite[0]][prerequisite[1]] = true;
         }
 
         floydWarshall();
         List<Boolean> answer = new ArrayList<>();
         for (int[] query : queries) {
-            if (distance[query[0]][query[1]] != Integer.MAX_VALUE) answer.add(true);
+            if (reachable[query[0]][query[1]]) answer.add(true);
             else answer.add(false);
         }
         return answer;
     }
 
     public void floydWarshall() {
-        for (int k = 0; k < distance.length; k++) {
-            for (int i = 0; i < distance.length; i++) {
-                for (int j = 0; j < distance.length; j++) {
-                    if (distance[i][k] == Integer.MAX_VALUE || distance[k][j] == Integer.MAX_VALUE) continue;
-                    distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+        for (int k = 0; k < reachable.length; k++) {
+            for (int i = 0; i < reachable.length; i++) {
+                for (int j = 0; j < reachable.length; j++) {
+                    if (reachable[i][k] && reachable[k][j]) reachable[i][j] = true;
                 }
             }
         }
