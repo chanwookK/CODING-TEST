a5c69c6cc9cf3a15a8198a0bebb012887c5bccf0
강찬욱 <114604267+chanwookK@users.noreply.github.com>
2026-07-20 18:18:18 +0900

    Time: 18 ms (52.72%), Space: 108 MB (27.18%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            if (set.contains(current)) return true;
+            set.add(current);
+        }
+        return false;
+    }
+}
